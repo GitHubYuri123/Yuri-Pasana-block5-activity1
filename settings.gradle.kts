c37bcd96bf819af1828653c73b7ab4f4bf3ca1ba
Yuri Pasana <yupa.pasana.up@phinmaed.com>
2023-12-09 21:17:57 +0800

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "YuriPasana-Block5-Quiz"
+include(":app")
+ 
