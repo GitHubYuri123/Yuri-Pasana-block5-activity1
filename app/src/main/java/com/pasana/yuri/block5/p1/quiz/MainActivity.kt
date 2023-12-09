d332c955c138050d4613735302f951b272376c4f
Yuri Pasana <yupa.pasana.up@phinmaed.com>
2023-12-09 22:42:08 +0800

    Initial Commit

--- app/src/main/java/com/pasana/yuri/block5/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/pasana/yuri/block5/p1/quiz/MainActivity.kt
@@ -1,15 +1,11 @@
-import android.os.Bundle
+package com.pasana.yuri.block5.p1.quiz
+
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        val loginFragment = LoginFragment()
-        fragmentTransaction.replace(R.id.fragment_container, loginFragment)
-        fragmentTransaction.commit()
     }
 }
